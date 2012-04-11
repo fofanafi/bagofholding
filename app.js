55f3f2eb0a5ac015001ebd394c58c5153f650042
Peter Kritikos <pkritiko@student.umass.edu>
2012-04-11 17:35:00 -0400

    Implemented user creation login/logout.
    User accounts persist across server reboot.
    
    Input validation for new user accounts is performed server-side.

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -11,14 +10,16 @@ var app = module.exports = express.createServer();
 // Configuration
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
-});
+		  app.set('views', __dirname + '/views');
+		  app.set('view engine', 'jade');
+		  app.use(express.bodyParser());
+		  app.use(express.cookieParser());
+		  app.use(express.session({secret: "cs391wp bag of holding"}));
+		  app.use(express.methodOverride());
+		  app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+		  app.use(app.router);
+		  app.use(express.static(__dirname + '/public'));
+	      });
 
 app.configure('development', function(){
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
@@ -29,9 +30,18 @@ app.configure('production', function(){
 });
 
 // Routes
-
 app.get('/', routes.index);
+app.get('/login', routes.login);
+app.get('/logout', routes.logout);
+app.get('/create', routes.create);
+app.get('/home', routes.home);
+ 
+app.post('/authenticate', routes.authenticate);
+app.post('/new_user', routes.newUser);
+
+routes.loadUsers();
 
 app.listen(3000, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
+
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,155 @@
+var fs = require("fs");
 
-/*
- * GET home page.
- */
+var loginRequired = function(routeFunction){
+    return function(req, res){
+	if(!req.session.username){
+	    res.redirect('/login');
+	    return;
+	}  else{
+	    routeFunction(req, res);
+	}
+    };
+}; 
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+
+//Dictionary mapping usernames to account information 
+var users = {};
+var dbFile = 'userdb.json';
+
+var storeUserData = function(cb) {
+    console.error('Writing bank records\n' + JSON.stringify(users));
+        fs.writeFile(dbFile, JSON.stringify(users), function (err){
+	if(err){
+	    console.error("Unable to write file: " + dbFile);
+	}
+	if(cb){
+	    cb();
+	}
+    });
+};
+
+var addUser =  function(name, password){
+    var validate = validateNewUser(name, password);
+
+    if(!validate.valid){
+	return validate.message;
+    }
+    
+    users[name] = { uname : name, 
+		    password : password
+		  };
+
+    storeUserData();
+    return "Created user " + name + ".";
+};
+
+var validateNewUser = function(name, password){
+    var result = {valid : true,
+	      message : ''};
+
+    if(!name || !password){
+	result.message = 'Must specify both name and password.\n';
+	return result;
+    }
+
+    if(name.length === 0){
+	result.valid = false;
+	result.message += 'Name can not have zero-length.\n';
+    }
+
+    if(password.length === 0){
+	result.valid = false;
+	result.message += 'Password cannot have zero-length.\n';
+    }
+    
+    if(users.name){
+	result.valid = false;
+	result.message += "User " + name + " already exists.\n" + 
+	    "You must specify a different user name.\n";
+    } 
+    
+    return result;
+};
+
+exports.index = function(req, res){ 
+    res.redirect('/login');
+};
+   
+exports.login = function(req, res) {
+    if(req.session && req.session.username){
+	res.redirect('/home');
+    }
+    else{ 
+	res.render('login',
+		   { title: 'Login', message:''}); 
+    }
+};
+
+
+exports.home = loginRequired(function(req, res) {
+     res.render('home', { title: 'Transcations',
+				 message :'Welcome ' + req.session.username});
+				 
+});
+
+
+
+exports.authenticate = function(req, res) {
+    var result; 
+    
+    if(!users[req.body.username]) { 
+	result = 'User ' + req.body.username + ' not found.';
+    }
+    else if(users[req.body.username].password === req.body.password){
+	req.session.username = req.body.username;
+	res.redirect('/home');
+	return;
+    }
+    else {
+	result = 'Incorrect password.';
+    }
+
+    res.render('login',
+	       { title : 'Login', message : result });
+};
+
+exports.logout = loginRequired(function(req, res) {
+    if(req.session.username){
+	req.session.destroy();
+    }
+    res.redirect('/login');
+});
+
+exports.newUser = function(req, res) {
+    var result = addUser(req.body.name, req.body.password, req.body.dateOfBirth);
+    res.render('login', 
+	       {title : 'Login', message : result});
+};
+
+exports.create = function(req, res) {
+    res.render('create', 
+	       { title: 'Create Account'});
+};
+
+exports.loadUsers = function(cb){
+    fs.readFile(dbFile, function(err, data){
+	if (err) {
+	    console.error("Unable to load file: " + dbFile);
+	}else{
+	    try{
+		users = JSON.parse(data);
+	    }
+	    catch(e){
+		console.error('Unable to parse JSON from file:\n' + dbFile + '\nDue to error:\n' + e);
+	    } 
+	}
+	if(!users){
+	    console.error("Data in " + dbFile + " is invalid. Reinitializing bank records.");
+	    users = {};
+	}
+
+	if(cb){
+	    cb(users);
+	}
+    });
 };
