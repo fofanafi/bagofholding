bc318b3e433ce7b7412da45b95542f24a749b133
Kyle Bishop <kybishop@gmail.com>
2012-04-11 20:47:59 -0400

    More reordering

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,10 @@
 var fs = require("fs");
 
-var loginRequired = function(routeFunction){
-    return function(req, res){
-	if(!req.session.username){
-	    res.redirect('/login');
-	    return;
-	}  else{
-	    routeFunction(req, res);
-	}
-    };
-}; 
-
-
 //Dictionary mapping usernames to account information 
 var users = {};
 var dbFile = 'userdb.json';
 
-var storeUserData = function(cb) {
-    fs.writeFile(dbFile, JSON.stringify(users), function (err){
-	    if(err){
-    	    console.error("Unable to write file: " + dbFile);
-    	}
-    	if(cb){
-    	    cb();
-    	}
-    });
-};
-
-var addUser =  function(name, password){
+function addUser(name, password){
     var validate = validateNewUser(name, password);
 
     if(!validate.valid){
@@ -45,7 +22,129 @@ var addUser =  function(name, password){
     return "Created user " + name + ".";
 };
 
-var validateNewUser = function(name, password){
+
+exports.authenticate = function(req, res) {
+    var result; 
+    
+    if(!users[req.body.username]) { 
+	result = 'User ' + req.body.username + ' not found.';
+    }
+    else if(users[req.body.username].password === req.body.password){
+	req.session.username = req.body.username;
+	res.redirect('/index');
+	return;
+    }
+    else {
+	result = 'Incorrect password.';
+    }
+
+    res.render('login',
+	       { title : 'Login', message : result });
+};
+
+
+exports.create = function(req, res) {
+    res.render('create', 
+	       { title: 'Create Account'});
+};
+
+
+exports.homepage = function(req, res) {
+     res.render('home', { title: 'Homepage'});
+};
+
+
+exports.index = function(req, res){ 
+    res.render('index', { title: 'User Home'});
+
+var fs = require('fs');
+
+
+fs.readdir('users/v', function (err, files) {
+if (err) console.error(err);
+console.log(files);
+});
+
+
+};
+
+
+exports.loadUsers = function(cb){
+    fs.readFile(dbFile, function(err, data){
+	if (err) {
+	    console.error("Unable to load file: " + dbFile);
+	}else{
+	    try{
+		users = JSON.parse(data);
+	    }
+	    catch(e){
+		console.error('Unable to parse JSON from file:\n' + dbFile + '\nDue to error:\n' + e);
+	    } 
+	}
+	if(!users){
+	    console.error("Data in " + dbFile + " is invalid. Reinitializing bank records.");
+	    users = {};
+	}
+
+	if(cb){
+	    cb(users);
+	}
+    });
+};
+
+
+exports.login = function(req, res) {
+    if(req.session && req.session.username){
+	res.redirect('/index');
+    }
+    else{ 
+	res.render('home',
+		   { title: 'Homepage'}); 
+    }
+};
+
+
+function loginRequired(routeFunction){
+    return function(req, res){
+	if(!req.session.username){
+	    res.redirect('/login');
+	    return;
+	}  else{
+	    routeFunction(req, res);
+	}
+    };
+}; 
+
+
+exports.logout = loginRequired(function(req, res) {
+    if(req.session.username){
+	req.session.destroy();
+    }
+    res.redirect('/login');
+});
+
+
+exports.newUser = function(req, res) {
+    var result = addUser(req.body.name, req.body.password, req.body.dateOfBirth);
+    
+    res.render('login', 
+	       {title : 'Login', message : result});
+};
+
+
+function storeUserData(cb) {
+    fs.writeFile(dbFile, JSON.stringify(users), function (err){
+	    if(err){
+    	    console.error("Unable to write file: " + dbFile);
+    	}
+    	if(cb){
+    	    cb();
+    	}
+    });
+};
+
+
+function validateNewUser(name, password){
     var result = {valid : true,
 	      message : ''};
 
@@ -71,95 +170,4 @@ var validateNewUser = function(name, password){
     } 
     
     return result;
-};
-
-exports.index = function(req, res){ 
-    res.render('index', { title: 'User Home'});
-
-var fs = require('fs');
-
-
-fs.readdir('users/v', function (err, files) {
-if (err) console.error(err);
-console.log(files);
-});
-
-
-};
-   
-exports.login = function(req, res) {
-    if(req.session && req.session.username){
-	res.redirect('/index');
-    }
-    else{ 
-	res.render('home',
-		   { title: 'Homepage'}); 
-    }
-};
-
-
-exports.homepage = function(req, res) {
-     res.render('home', { title: 'Homepage'});
-};
-
-
-exports.authenticate = function(req, res) {
-    var result; 
-    
-    if(!users[req.body.username]) { 
-	result = 'User ' + req.body.username + ' not found.';
-    }
-    else if(users[req.body.username].password === req.body.password){
-	req.session.username = req.body.username;
-	res.redirect('/index');
-	return;
-    }
-    else {
-	result = 'Incorrect password.';
-    }
-
-    res.render('login',
-	       { title : 'Login', message : result });
-};
-
-exports.logout = loginRequired(function(req, res) {
-    if(req.session.username){
-	req.session.destroy();
-    }
-    res.redirect('/login');
-});
-
-exports.newUser = function(req, res) {
-    var result = addUser(req.body.name, req.body.password, req.body.dateOfBirth);
-    
-    res.render('login', 
-	       {title : 'Login', message : result});
-};
-
-exports.create = function(req, res) {
-    res.render('create', 
-	       { title: 'Create Account'});
-};
-
-exports.loadUsers = function(cb){
-    fs.readFile(dbFile, function(err, data){
-	if (err) {
-	    console.error("Unable to load file: " + dbFile);
-	}else{
-	    try{
-		users = JSON.parse(data);
-	    }
-	    catch(e){
-		console.error('Unable to parse JSON from file:\n' + dbFile + '\nDue to error:\n' + e);
-	    } 
-	}
-	if(!users){
-	    console.error("Data in " + dbFile + " is invalid. Reinitializing bank records.");
-	    users = {};
-	}
-
-	if(cb){
-	    cb(users);
-	}
-    });
 };
