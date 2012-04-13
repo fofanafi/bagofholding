f2040b5ed72ec52c5e4925e72387a74df63b16b9
Kyle Bishop <kybishop@gmail.com>
2012-04-13 03:15:26 -0400

    More fancy effects on the homepage

--- public/javascript/homepage.js
+++ public/javascript/homepage.js
@@ -1,23 +1,26 @@
-// Sets the homepage form to login
-function login() {
-    var form = document.getElementById("form");
-    form.action = "authenticate";
-
-    var loginTab = document.getElementById("login");
-    var createTab = document.getElementById("create");
-
-    loginTab.style.boxShadow = "";
-    createTab.style.boxShadow = "inset 2px -2px 5px #aaa";
-};
-
 // Sets the homepage form to create a new user
 function create() {
-    var form = document.getElementById("form");
-    form.action = "new_user";
-
-    var loginTab = document.getElementById("login");
-    var createTab = document.getElementById("create");
-
-    loginTab.style.boxShadow = "inset -2px -2px 5px #aaa";
-    createTab.style.boxShadow = "none";
+    document.getElementById("form").action = "new_user";
+    document.getElementById("submitForm").value = "Create new bag of holding"
+    document.getElementById("login").style.boxShadow = "inset -2px -2px 5px #aaa";
+    document.getElementById("create").style.boxShadow = "none";
 }
+
+
+// Sets the homepage form to login
+function login() {
+    document.getElementById("form").action = "authenticate";
+    document.getElementById("submitForm").value = "Open my bag of holding"
+    document.getElementById("login").style.boxShadow = "";
+    document.getElementById("create").style.boxShadow = "inset 2px -2px 5px #aaa";
+};
+
+
+function removePassword() {
+    document.getElementById("password").value = "";
+};
+
+
+function removeUsername() {
+    document.getElementById("username").value = "";
+};
