678fc67b7e80b28cd1d68b4f9c294aa3e9dff298
Kyle Bishop <kybishop@gmail.com>
2012-04-13 02:30:02 -0400

    Ladies and gentlemen, we have tabs B)

--- public/javascript/homepage.js
+++ public/javascript/homepage.js
@@ -2,10 +2,22 @@
 function login() {
     var form = document.getElementById("form");
     form.action = "authenticate";
+
+    var loginTab = document.getElementById("login");
+    var createTab = document.getElementById("create");
+
+    loginTab.style.boxShadow = "";
+    createTab.style.boxShadow = "inset 2px -2px 5px #aaa";
 };
 
 // Sets the homepage form to create a new user
 function create() {
     var form = document.getElementById("form");
     form.action = "new_user";
+
+    var loginTab = document.getElementById("login");
+    var createTab = document.getElementById("create");
+
+    loginTab.style.boxShadow = "inset -2px -2px 5px #aaa";
+    createTab.style.boxShadow = "none";
 }
